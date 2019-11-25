0af0878ff7f2996066ed56558c69d263e626efae
bigname <tuanhiep.tran88@gmail.com>
2019-11-25 03:25:02 -0700

    Implement Deep Neural Network

---
+++ DeepNeuralNetwork.py
@@ -0,0 +1,15 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases_v4a import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+%load_ext autoreload
+%autoreload 2
+
+np.random.seed(1)
