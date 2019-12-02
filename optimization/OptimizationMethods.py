0e831f7280222e10c89d5d3eb0767e5a42828cc0
bigname <tuanhiep.tran88@gmail.com>
2019-12-01 18:15:11 -0700

    Optimization for gradient descent using mini-batch instead of using batch gradient descent or stochastic gradient descent

---
+++ optimization/OptimizationMethods.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils_v1a import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils_v1a import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from testCases import *
+
+% matplotlib
+inline
+plt.rcParams['figure.figsize'] = (7.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+# 1 - Gradient Descent
+
+# GRADED FUNCTION: update_parameters_with_gd
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    """
+    Update parameters using one step of gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters to be updated:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients to update each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    learning_rate -- the learning rate, scalar.
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for l in range(L):
+        ### START CODE HERE ### (approx. 2 lines)
+        parameters["W" + str(l + 1)] = parameters['W' + str(l + 1)] - learning_rate * grads['dW' + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters['b' + str(l + 1)] - learning_rate * grads['db' + str(l + 1)]
+        ### END CODE HERE ###
+
+    return parameters
+
+
+parameters, grads, learning_rate = update_parameters_with_gd_test_case()
+
+parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+print("W1 =\n" + str(parameters["W1"]))
+print("b1 =\n" + str(parameters["b1"]))
+print("W2 =\n" + str(parameters["W2"]))
+print("b2 =\n" + str(parameters["b2"]))
+
+# What you should remember:
+# The difference between gradient descent, mini-batch gradient descent and stochastic gradient descent is the number of
+# examples you use to perform one update step.
+# You have to tune a learning rate hyperparameter  α .
+# With a well-turned mini-batch size, usually it outperforms either gradient descent or stochastic gradient descent
+# (particularly when the training set is large).
+
+
+# 2 - Mini-Batch Gradient descent
+
+
+# GRADED FUNCTION: random_mini_batches
+
+def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
+    """
+    Creates a list of random minibatches from (X, Y)
+
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    mini_batch_size -- size of the mini-batches, integer
+
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+
+    np.random.seed(seed)  # To make your "random" minibatches the same as ours
+    m = X.shape[1]  # number of training examples
+    mini_batches = []
+
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1, m))
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(
+        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[:, k * mini_batch_size: (k + 1) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size: (k + 1) * mini_batch_size]
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[:, m - num_complete_minibatches * mini_batch_size: m]
+        mini_batch_Y = shuffled_Y[:, m - num_complete_minibatches * mini_batch_size: m]
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+X_assess, Y_assess, mini_batch_size = random_mini_batches_test_case()
+mini_batches = random_mini_batches(X_assess, Y_assess, mini_batch_size)
+
+print ("shape of the 1st mini_batch_X: " + str(mini_batches[0][0].shape))
+print ("shape of the 2nd mini_batch_X: " + str(mini_batches[1][0].shape))
+print ("shape of the 3rd mini_batch_X: " + str(mini_batches[2][0].shape))
+print ("shape of the 1st mini_batch_Y: " + str(mini_batches[0][1].shape))
+print ("shape of the 2nd mini_batch_Y: " + str(mini_batches[1][1].shape))
+print ("shape of the 3rd mini_batch_Y: " + str(mini_batches[2][1].shape))
+print ("mini batch sanity check: " + str(mini_batches[0][0][0][0:3]))
+
+
+# What you should remember:
+# Shuffling and Partitioning are the two steps required to build mini-batches
+# Powers of two are often chosen to be the mini-batch size, e.g., 16, 32, 64, 128.
