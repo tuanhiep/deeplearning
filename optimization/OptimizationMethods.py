c260dc4004b91be1bf66bf6746807ac1e48d951e
bigname <tuanhiep.tran88@gmail.com>
2019-12-01 18:51:13 -0700

    Optimization for gradient descent using momentum

--- optimization/OptimizationMethods.py
+++ optimization/OptimizationMethods.py
@@ -129,3 +129,102 @@ print ("mini batch sanity check: " + str(mini_batches[0][0][0][0:3]))
 # What you should remember:
 # Shuffling and Partitioning are the two steps required to build mini-batches
 # Powers of two are often chosen to be the mini-batch size, e.g., 16, 32, 64, 128.
+
+
+# GRADED FUNCTION: initialize_velocity
+
+def initialize_velocity(parameters):
+    """
+    Initializes the velocity as a python dictionary with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL"
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+
+    Returns:
+    v -- python dictionary containing the current velocity.
+                    v['dW' + str(l)] = velocity of dWl
+                    v['db' + str(l)] = velocity of dbl
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v = {}
+
+    # Initialize velocity
+    for l in range(L):
+        ### START CODE HERE ### (approx. 2 lines)
+        v["dW" + str(l + 1)] = np.zeros(parameters['W' + str(l + 1)].shape)
+        v["db" + str(l + 1)] = np.zeros((parameters['b' + str(l + 1)].shape))
+        ### END CODE HERE ###
+
+    return v
+
+parameters = initialize_velocity_test_case()
+
+v = initialize_velocity(parameters)
+print("v[\"dW1\"] =\n" + str(v["dW1"]))
+print("v[\"db1\"] =\n" + str(v["db1"]))
+print("v[\"dW2\"] =\n" + str(v["dW2"]))
+print("v[\"db2\"] =\n" + str(v["db2"]))
+
+
+# GRADED FUNCTION: update_parameters_with_momentum
+
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    """
+    Update parameters using Momentum
+
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- python dictionary containing the current velocity:
+                    v['dW' + str(l)] = ...
+                    v['db' + str(l)] = ...
+    beta -- the momentum hyperparameter, scalar
+    learning_rate -- the learning rate, scalar
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    v -- python dictionary containing your updated velocities
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Momentum update for each parameter
+    for l in range(L):
+        ### START CODE HERE ### (approx. 4 lines)
+        # compute velocities
+        v["dW" + str(l + 1)] = beta * v["dW" + str(l + 1)] + (1 - beta) * grads['dW' + str(l + 1)]
+        v["db" + str(l + 1)] = beta * v["db" + str(l + 1)] + (1 - beta) * grads['db' + str(l + 1)]
+        # update parameters
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * v["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * v["db" + str(l + 1)]
+        ### END CODE HERE ###
+
+    return parameters, v
+
+parameters, grads, v = update_parameters_with_momentum_test_case()
+
+parameters, v = update_parameters_with_momentum(parameters, grads, v, beta = 0.9, learning_rate = 0.01)
+print("W1 = \n" + str(parameters["W1"]))
+print("b1 = \n" + str(parameters["b1"]))
+print("W2 = \n" + str(parameters["W2"]))
+print("b2 = \n" + str(parameters["b2"]))
+print("v[\"dW1\"] = \n" + str(v["dW1"]))
+print("v[\"db1\"] = \n" + str(v["db1"]))
+print("v[\"dW2\"] = \n" + str(v["dW2"]))
+print("v[\"db2\"] = v" + str(v["db2"]))
+
+#
+# What you should remember:
+# Momentum takes past gradients into account to smooth out the steps of gradient descent. It can be applied with batch gradient descent, mini-batch gradient descent or stochastic gradient descent.
+# You have to tune a momentum hyperparameter β and a learning rate α .
+
+
+# 4 - Adam
