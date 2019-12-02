74fedfedc669257834ae3504aaa91e38ef106476
bigname <tuanhiep.tran88@gmail.com>
2019-12-02 15:37:48 -0700

    Create linear function with tensorflow

---
+++ TensorflowPractice.py
@@ -0,0 +1,82 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+%matplotlib inline
+np.random.seed(1)
+
+# Exploring the Tensorflow Library
+
+y_hat = tf.constant(36, name='y_hat')            # Define y_hat constant. Set to 36.
+y = tf.constant(39, name='y')                    # Define y. Set to 39
+
+loss = tf.Variable((y - y_hat)**2, name='loss')  # Create a variable for the loss
+
+init = tf.global_variables_initializer()         # When init is run later (session.run(init)),
+                                                 # the loss variable will be initialized and ready to be computed
+with tf.Session() as session:                    # Create a session and print the output
+    session.run(init)                            # Initializes the variables
+    print(session.run(loss))                     # Prints the loss
+
+a = tf.constant(2)
+b = tf.constant(10)
+c = tf.multiply(a,b)
+print(c)
+
+sess = tf.Session()
+print(sess.run(c))
+
+# To summarize, remember to initialize your variables, create a session and run the operations inside the session.
+
+# Change the value of x in the feed_dict
+
+x = tf.placeholder(tf.int64, name = 'x')
+print(sess.run(2 * x, feed_dict = {x: 3}))
+sess.close()
+
+# Linear function
+
+# GRADED FUNCTION: linear_function
+
+def linear_function():
+    """
+    Implements a linear function:
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns:
+    result -- runs the session for Y = WX + b
+    """
+
+    np.random.seed(1)
+
+    """
+    Note, to ensure that the "random" numbers generated match the expected results,
+    please create the variables in the order given in the starting code below.
+    (Do not re-arrange the order).
+    """
+    ### START CODE HERE ### (4 lines of code)
+    X = tf.constant(np.random.randn(3, 1), name="X")
+    W = tf.constant(np.random.randn(4, 3), name="W")
+    b = tf.constant(np.random.randn(4, 1), name="b")
+    Y = tf.add(tf.matmul(W, X), b)
+    ### END CODE HERE ###
+
+    # Create the session using tf.Session() and run it with sess.run(...) on the variable you want to calculate
+
+    ### START CODE HERE ###
+    sess = tf.Session()
+    result = sess.run(Y)
+    ### END CODE HERE ###
+
+    # close the session
+    sess.close()
+
+    return result
+
+print( "result = \n" + str(linear_function()))
+
