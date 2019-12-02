978c7bf884287e09fad498bd95ce1db75adb0202
bigname <tuanhiep.tran88@gmail.com>
2019-12-01 21:34:13 -0700

    Optimization for gradient descent using mini-batch,momentum, adam mode

--- optimization/OptimizationMethods.py
+++ optimization/OptimizationMethods.py
@@ -228,3 +228,284 @@ print("v[\"db2\"] = v" + str(v["db2"]))
 
 
 # 4 - Adam
+# GRADED FUNCTION: initialize_adam
+
+def initialize_adam(parameters):
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL"
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+
+    Returns:
+    v -- python dictionary that will contain the exponentially weighted average of the gradient.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v = {}
+    s = {}
+
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(L):
+        ### START CODE HERE ### (approx. 4 lines)
+        v["dW" + str(l + 1)] = np.zeros(parameters["W" + str(l + 1)].shape)
+        v["db" + str(l + 1)] = np.zeros(parameters["b" + str(l + 1)].shape)
+        s["dW" + str(l + 1)] = np.zeros(parameters["W" + str(l + 1)].shape)
+        s["db" + str(l + 1)] = np.zeros(parameters["b" + str(l + 1)].shape)
+    ### END CODE HERE ###
+
+    return v, s
+
+parameters = initialize_adam_test_case()
+
+v, s = initialize_adam(parameters)
+print("v[\"dW1\"] = \n" + str(v["dW1"]))
+print("v[\"db1\"] = \n" + str(v["db1"]))
+print("v[\"dW2\"] = \n" + str(v["dW2"]))
+print("v[\"db2\"] = \n" + str(v["db2"]))
+print("s[\"dW1\"] = \n" + str(s["dW1"]))
+print("s[\"db1\"] = \n" + str(s["db1"]))
+print("s[\"dW2\"] = \n" + str(s["dW2"]))
+print("s[\"db2\"] = \n" + str(s["db2"]))
+
+
+# GRADED FUNCTION: update_parameters_with_adam
+
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
+                                beta1=0.9, beta2=0.999, epsilon=1e-8):
+    """
+    Update parameters using Adam
+
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    learning_rate -- the learning rate, scalar.
+    beta1 -- Exponential decay hyperparameter for the first moment estimates
+    beta2 -- Exponential decay hyperparameter for the second moment estimates
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v_corrected = {}  # Initializing first moment estimate, python dictionary
+    s_corrected = {}  # Initializing second moment estimate, python dictionary
+
+    # Perform Adam update on all parameters
+    for l in range(L):
+        # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
+        ### START CODE HERE ### (approx. 2 lines)
+        v["dW" + str(l + 1)] = beta1 * v["dW" + str(l + 1)] + (1 - beta1) * grads['dW' + str(l + 1)]
+        v["db" + str(l + 1)] = beta1 * v["db" + str(l + 1)] + (1 - beta1) * grads['db' + str(l + 1)]
+        ### END CODE HERE ###
+
+        # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        v_corrected["dW" + str(l + 1)] = v["dW" + str(l + 1)] / (1 - np.power(beta1, t))
+        v_corrected["db" + str(l + 1)] = v["db" + str(l + 1)] / (1 - np.power(beta1, t))
+        ### END CODE HERE ###
+
+        # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
+        ### START CODE HERE ### (approx. 2 lines)
+        s["dW" + str(l + 1)] = beta2 * s["dW" + str(l + 1)] + (1 - beta2) * np.power(grads['dW' + str(l + 1)], 2)
+        s["db" + str(l + 1)] = beta2 * s["db" + str(l + 1)] + (1 - beta2) * np.power(grads['db' + str(l + 1)], 2)
+        ### END CODE HERE ###
+
+        # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        s_corrected["dW" + str(l + 1)] = s["dW" + str(l + 1)] / (1 - np.power(beta1, t))
+        s_corrected["db" + str(l + 1)] = s["db" + str(l + 1)] / (1 - np.power(beta2, t))
+        ### END CODE HERE ###
+
+        # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
+        ### START CODE HERE ### (approx. 2 lines)
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - (
+                    learning_rate * v_corrected["dW" + str(l + 1)]) / (
+                                                   np.power(s_corrected["dW" + str(l + 1)], 0.5) + epsilon)
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - (
+                    learning_rate * v_corrected["db" + str(l + 1)]) / (
+                                                   np.power(s_corrected["db" + str(l + 1)], 0.5) + epsilon)
+        ### END CODE HERE ###
+
+    return parameters, v, s
+
+parameters, grads, v, s = update_parameters_with_adam_test_case()
+parameters, v, s  = update_parameters_with_adam(parameters, grads, v, s, t = 2)
+
+print("W1 = \n" + str(parameters["W1"]))
+print("b1 = \n" + str(parameters["b1"]))
+print("W2 = \n" + str(parameters["W2"]))
+print("b2 = \n" + str(parameters["b2"]))
+print("v[\"dW1\"] = \n" + str(v["dW1"]))
+print("v[\"db1\"] = \n" + str(v["db1"]))
+print("v[\"dW2\"] = \n" + str(v["dW2"]))
+print("v[\"db2\"] = \n" + str(v["db2"]))
+print("s[\"dW1\"] = \n" + str(s["dW1"]))
+print("s[\"db1\"] = \n" + str(s["db1"]))
+print("s[\"dW2\"] = \n" + str(s["dW2"]))
+print("s[\"db2\"] = \n" + str(s["db2"]))
+
+
+train_X, train_Y = load_dataset()
+
+
+def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=10000, print_cost=True):
+    """
+    3-layer neural network model which can be run in different optimizer modes.
+
+    Arguments:
+    X -- input data, of shape (2, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    layers_dims -- python list, containing the size of each layer
+    learning_rate -- the learning rate, scalar.
+    mini_batch_size -- the size of a mini batch
+    beta -- Momentum hyperparameter
+    beta1 -- Exponential decay hyperparameter for the past gradients estimates
+    beta2 -- Exponential decay hyperparameter for the past squared gradients estimates
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+    num_epochs -- number of epochs
+    print_cost -- True to print the cost every 1000 epochs
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+
+    L = len(layers_dims)  # number of layers in the neural networks
+    costs = []  # to keep track of the cost
+    t = 0  # initializing the counter required for Adam update
+    seed = 10  # For grading purposes, so that your "random" minibatches are the same as ours
+    m = X.shape[1]  # number of training examples
+
+    # Initialize parameters
+    parameters = initialize_parameters(layers_dims)
+
+    # Initialize the optimizer
+    if optimizer == "gd":
+        pass  # no initialization required for gradient descent
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+
+    # Optimization loop
+    for i in range(num_epochs):
+
+        # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+        cost_total = 0
+
+        for minibatch in minibatches:
+
+            # Select a minibatch
+            (minibatch_X, minibatch_Y) = minibatch
+
+            # Forward propagation
+            a3, caches = forward_propagation(minibatch_X, parameters)
+
+            # Compute cost and add to the cost total
+            cost_total += compute_cost(a3, minibatch_Y)
+
+            # Backward propagation
+            grads = backward_propagation(minibatch_X, minibatch_Y, caches)
+
+            # Update parameters
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1  # Adam counter
+                parameters, v, s = update_parameters_with_adam(parameters, grads, v, s,
+                                                               t, learning_rate, beta1, beta2, epsilon)
+        cost_avg = cost_total / m
+
+        # Print the cost every 1000 epoch
+        if print_cost and i % 1000 == 0:
+            print("Cost after epoch %i: %f" % (i, cost_avg))
+        if print_cost and i % 100 == 0:
+            costs.append(cost_avg)
+
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+# Mini-batch Gradient descent
+
+# train 3-layer model
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, optimizer = "gd")
+
+# Predict
+predictions = predict(train_X, train_Y, parameters)
+
+# Plot decision boundary
+plt.title("Model with Gradient Descent optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5,2.5])
+axes.set_ylim([-1,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+# Mini-batch gradient descent with momentum
+
+# train 3-layer model
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, beta = 0.9, optimizer = "momentum")
+
+# Predict
+predictions = predict(train_X, train_Y, parameters)
+
+# Plot decision boundary
+plt.title("Model with Momentum optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5,2.5])
+axes.set_ylim([-1,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+# Mini-batch with Adam mode
+# train 3-layer model
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, optimizer = "adam")
+
+# Predict
+predictions = predict(train_X, train_Y, parameters)
+
+# Plot decision boundary
+plt.title("Model with Adam optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5,2.5])
+axes.set_ylim([-1,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+# Momentum usually helps, but given the small learning rate and the simplistic dataset, its impact is almost negligeable. Also, the huge oscillations you see in the cost come from the fact that some minibatches are more difficult thans others for the optimization algorithm.
+# Adam on the other hand, clearly outperforms mini-batch gradient descent and Momentum. If you run the model for more epochs on this simple dataset, all three methods will lead to very good results. However, you've seen that Adam converges a lot faster.
+# Some advantages of Adam include:
+# Relatively low memory requirements (though higher than gradient descent and gradient descent with momentum)
+# Usually works well even with little tuning of hyperparameters (except  α
+#  )
