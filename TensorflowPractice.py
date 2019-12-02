96c540c5ad69019c57e15f4358077d5e07c8f965
bigname <tuanhiep.tran88@gmail.com>
2019-12-02 16:27:05 -0700

    use one-hot encoding with tensorflow

--- TensorflowPractice.py
+++ TensorflowPractice.py
@@ -80,3 +80,148 @@ def linear_function():
 
 print( "result = \n" + str(linear_function()))
 
+# Computing the sigmoid
+
+# GRADED FUNCTION: sigmoid
+
+def sigmoid(z):
+    """
+    Computes the sigmoid of z
+
+    Arguments:
+    z -- input value, scalar or vector
+
+    Returns:
+    results -- the sigmoid of z
+    """
+
+    ### START CODE HERE ### ( approx. 4 lines of code)
+    # Create a placeholder for x. Name it 'x'.
+    x = tf.placeholder(tf.float32, name='x')
+
+    # compute sigmoid(x)
+    sigmoid = tf.sigmoid(x)
+
+    # Create a session, and run it. Please use the method 2 explained above.
+    # You should use a feed_dict to pass z's value to x.
+
+    # Run session and call the output "result"
+    with tf.Session() as sess:
+        # run the variables initialization (if needed), run the operations
+        result = sess.run(sigmoid, feed_dict={x: z})
+
+    ### END CODE HERE ###
+
+    return result
+
+print ("sigmoid(0) = " + str(sigmoid(0)))
+print ("sigmoid(12) = " + str(sigmoid(12)))
+
+
+# To summarize:
+# Create placeholders
+# Specify the computation graph corresponding to operations you want to compute
+# Create the session
+# Run the session, using a feed dictionary if necessary to specify placeholder variables' values.
+
+# Computing the Cost
+
+
+# GRADED FUNCTION: cost
+
+def cost(logits, labels):
+    """
+    Computes the cost using the sigmoid cross entropy
+    
+    Arguments:
+    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
+    labels -- vector of labels y (1 or 0)
+
+    Note: What we've been calling "z" and "y" in this class are respectively called "logits" and "labels"
+    in the TensorFlow documentation. So logits will feed into z, and labels into y.
+    
+    Returns:
+    cost -- runs the session of the cost (formula (2))
+    """
+
+    ### START CODE HERE ###
+
+    # Create the placeholders for "logits" (z) and "labels" (y) (approx. 2 lines)
+    z = tf.placeholder(tf.float32, shape=logits.shape, name="z")
+    y = tf.placeholder(tf.float32, shape=labels.shape, name="y")
+
+    # Use the loss function (approx. 1 line)
+    cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=z, labels=y)
+
+    # Create a session (approx. 1 line). See method 1 above.
+    sess = tf.Session()
+
+    # Run the session (approx. 1 line).
+    cost = sess.run(cost, feed_dict={z: logits, y: labels})
+
+    # Close the session (approx. 1 line). See method 1 above.
+    sess.close()
+
+    ### END CODE HERE ###
+
+    return cost
+
+logits = np.array([0.2,0.4,0.7,0.9])
+
+cost = cost(logits, np.array([0,0,1,1]))
+print ("cost = " + str(cost))
+
+# Using One Hot encodings
+
+
+# GRADED FUNCTION: one_hot_matrix
+
+def one_hot_matrix(labels, C):
+    """
+    Creates a matrix where the i-th row corresponds to the ith class number and the jth column
+                     corresponds to the jth training example. So if example j had a label i. Then entry (i,j)
+                     will be 1.
+
+    Arguments:
+    labels -- vector containing the labels
+    C -- number of classes, the depth of the one hot dimension
+
+    Returns:
+    one_hot -- one hot matrix
+    """
+
+    ### START CODE HERE ###
+
+    # Create a tf.constant equal to C (depth), name it 'C'. (approx. 1 line)
+    C = tf.constant(C)
+
+    # Use tf.one_hot, be careful with the axis (approx. 1 line)
+    one_hot_matrix = tf.one_hot(labels, depth=C, axis=0)
+
+    # Create the session (approx. 1 line)
+    sess = tf.Session()
+
+    # Run the session (approx. 1 line)
+    one_hot = sess.run(one_hot_matrix)
+
+    # Close the session (approx. 1 line). See method 1 above.
+    sess.close()
+
+    ### END CODE HERE ###
+
+    return one_hot
+
+labels = np.array([1,2,3,0,2,1])
+one_hot = one_hot_matrix(labels, C = 4)
+print ("one_hot = \n" + str(one_hot))
+
+
+
+
+
+
+
+
+
+
+
