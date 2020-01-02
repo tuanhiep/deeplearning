be15c8851e3b658305e30007a77f18d29796d507
bigname <tuanhiep.tran88@gmail.com>
2020-01-02 15:31:47 -0700

    Finish forward convolution step

--- convolution/ConvolutionModel.py
+++ convolution/ConvolutionModel.py
@@ -71,7 +71,7 @@ def conv_single_step(a_slice_prev, W, b):
     # Sum over all entries of the volume s.
     Z = np.sum(s)
     # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
-    Z = Z + b.astype(float)
+    Z = Z + float(b)
     ### END CODE HERE ###
 
     return Z
@@ -83,4 +83,144 @@ b = np.random.randn(1, 1, 1)
 
 Z = conv_single_step(a_slice_prev, W, b)
 print("Z =", Z)
+
+
+# GRADED FUNCTION: conv_forward
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+
+    Arguments:
+    A_prev -- output activations of the previous layer,
+        numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+    """
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from A_prev's shape (≈1 line)
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve dimensions from W's shape (≈1 line)
+    (f, f, n_C_prev, n_C) = W.shape
+
+    # Retrieve information from "hparameters" (≈2 lines)
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+
+    # Compute the dimensions of the CONV output volume using the formula given above.
+    # Hint: use int() to apply the 'floor' operation. (≈2 lines)
+    n_H = int((n_H_prev + 2 * pad - f) / stride) + 1
+    n_W = int((n_W_prev + 2 * pad - f) / stride) + 1
+
+    # Initialize the output volume Z with zeros. (≈1 line)
+    Z = np.zeros((m, n_H, n_W, n_C))
+
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad = zero_pad(A_prev, pad)
+
+    for i in range(0, m):  # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i, :, :, :]  # Select ith training example's padded activation
+        for h in range(0, n_H):  # loop over vertical axis of the output volume
+            # Find the vertical start and end of the current "slice" (≈2 lines)
+            vert_start = h
+            vert_end = h + f
+
+            for w in range(0, n_W):  # loop over horizontal axis of the output volume
+                # Find the horizontal start and end of the current "slice" (≈2 lines)
+                horiz_start = w
+                horiz_end = w + f
+
+                for c in range(0, n_C):  # loop over channels (= #filters) of the output volume
+
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
+                    weights = W[:, :, :, c]
+                    biases = b[:, :, :, c]
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, weights, biases)
+
+    ### END CODE HERE ###
+
+    # Making sure your output shape is correct
+    assert (Z.shape == (m, n_H, n_W, n_C))
+
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+
+    return Z, cache
+
+
+# GRADED FUNCTION: pool_forward
+
+def pool_forward(A_prev, hparameters, mode="max"):
+    """
+    Implements the forward pass of the pooling layer
+
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters
+    """
+
+    # Retrieve dimensions from the input shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve hyperparameters from "hparameters"
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+
+    # Define the dimensions of the output
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+
+    # Initialize output matrix A
+    A = np.zeros((m, n_H, n_W, n_C))
+
+    ### START CODE HERE ###
+    for i in range(0, m):  # loop over the training examples
+        for h in range(0, n_H):  # loop on the vertical axis of the output volume
+            # Find the vertical start and end of the current "slice" (≈2 lines)
+            vert_start = h
+            vert_end = h + f
+
+            for w in range(0, n_W):  # loop on the horizontal axis of the output volume
+                # Find the vertical start and end of the current "slice" (≈2 lines)
+                horiz_start = w
+                horiz_end = w + f
+
+                for c in range(0, n_C):  # loop over the channels of the output volume
+
+                    # Use the corners to define the current slice on the ith training example of A_prev, channel c. (≈1 line)
+                    a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+
+                    # Compute the pooling operation on the slice.
+                    # Use an if statement to differentiate the modes.
+                    # Use np.max and np.mean.
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+    ### END CODE HERE ###
+
+    # Store the input and hparameters in "cache" for pool_backward()
+    cache = (A_prev, hparameters)
+
+    # Making sure your output shape is correct
+    assert (A.shape == (m, n_H, n_W, n_C))
+
+    return A, cache
 
