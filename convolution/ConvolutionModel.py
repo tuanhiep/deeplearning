3680da606c730768cfdfd454fae1bb70b9b31434
bigname <tuanhiep.tran88@gmail.com>
2020-01-01 21:48:17 -0700

    Single step of convolution with zero-padding and apply one filter on a single slice

--- convolution/ConvolutionModel.py
+++ convolution/ConvolutionModel.py
@@ -12,3 +12,75 @@ plt.rcParams['image.cmap'] = 'gray'
 
 np.random.seed(1)
 
+
+# GRADED FUNCTION: zero_pad
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image,
+    as illustrated in Figure 1.
+
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+
+    ### START CODE HERE ### (≈ 1 line)
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), mode='constant', constant_values=(0, 0))
+    ### END CODE HERE ###
+
+    return X_pad
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =\n", x.shape)
+print ("x_pad.shape =\n", x_pad.shape)
+print ("x[1,1] =\n", x[1,1])
+print ("x_pad[1,1] =\n", x_pad[1,1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
+
+
+# GRADED FUNCTION: conv_single_step
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
+    of the previous layer.
+
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+
+    Returns:
+    Z -- a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+
+    ### START CODE HERE ### (≈ 2 lines of code)
+    # Element-wise product between a_slice_prev and W. Do not add the bias yet.
+    s = a_slice_prev * W
+    # Sum over all entries of the volume s.
+    Z = np.sum(s)
+    # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+    Z = Z + b.astype(float)
+    ### END CODE HERE ###
+
+    return Z
+
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+
