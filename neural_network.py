b029fd65c1f4322a3de5516f094bf413b3c7d02d
bigname <tuanhiep.tran88@gmail.com>
2020-05-24 01:06:52 -0600

    finish convolutional_model.py

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,13 @@
 # Package imports
 import numpy as np
 import matplotlib.pyplot as plt
-from testCases_v2 import *
+# from testCases_v2 import *
 import sklearn
 import sklearn.datasets
 import sklearn.linear_model
-from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+# from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 
-%matplotlib inline
+# %matplotlib inline
 
 np.random.seed(1) # set a seed so that the results are consistent
 X, Y = load_planar_dataset()
